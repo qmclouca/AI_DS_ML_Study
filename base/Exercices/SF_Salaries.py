a84cb863acd55a566933b3775048750dc5cf023c
Rodolfo Bortoluzzi <qmclouca@gmail.com>
2023-01-30 07:59:10 -0300

    exercices SF salaries

--- base/Exercices/SF_Salaries.py
+++ base/Exercices/SF_Salaries.py
@@ -5,7 +5,31 @@ import numpy as np
 df = pd.read_csv('/home/odolfo/AI/AI_DS_ML_Study/DB/SF_Salaries/Salaries.csv',sep=',')
 print(df.head())
 print(df.info())
-df_BaseSalary = df[df['BasePay'].str.contains('Not Provided')==False]
-print("base salary:")
-print(df_BaseSalary.info())
-df_BaseSalary = pd.to_numeric(df_BaseSalary['BasePay'], downcast='float')
+
+data = df[df['EmployeeName'] != 'Not Provided']
+data = data[data['BasePay'] != 'Not Provided']
+data = data[data['JobTitle'] != 'Not Provided']
+
+data.reset_index(inplace=True, drop=True)
+
+data['BasePay'] = data['BasePay'].astype(float)
+data['OvertimePay'] = data['OvertimePay'].astype(float)
+data['OtherPay'] = data['OtherPay'].astype(float)
+data = data.drop(['Id', 'Notes', 'Agency', 'Status'], axis=1)
+
+print(data.dtypes)
+print(data.shape)
+
+print(f"DataFrame has {data.shape[0]} rows and {data.shape[1]} columns")
+
+print(f"Base Salary mean is: {data['BasePay'].mean()}")
+print(f"Base Salary median is: {data['BasePay'].median()}")
+print(f"Base Salary standard deviation is: {data['BasePay'].std()}")
+print(f"Base Salary variance is: {data['BasePay'].var()}")
+print(f"Base Salary max is: {data['BasePay'].max()}")
+print(f"Base Salary min is: {data['BasePay'].min()}")
+
+print(data.describe().T)
+
+
+
