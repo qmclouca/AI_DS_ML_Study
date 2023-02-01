8ff7eaf215a630fd94ea6b3d2064c7d49a3480bb
Rodolfo Bortoluzzi <qmclouca@gmail.com>
2023-02-01 07:28:02 -0300

    play with dataframes

--- base/Exercices/SF_Salaries.py
+++ base/Exercices/SF_Salaries.py
@@ -39,10 +39,33 @@ print(f"Overtime Salary min is: {data['OvertimePay'].min()}")
 print(data[data['EmployeeName'] =='JOSEPH DRISCOLL']['JobTitle'])
 print(data[data['EmployeeName'] =='JOSEPH DRISCOLL']['TotalPayBenefits'])
 
-print(data[data['TotalPayBenefits'] == data['TotalPayBenefits'].max()]['EmployeeName'])
+print(data[data['TotalPayBenefits'] == data['TotalPayBenefits'].max()]) # Returns the row with the highest salary
 
+print(data[data['TotalPayBenefits'] == data['TotalPayBenefits'].max()]['EmployeeName']) # Returns the name of the employee with the highest salary
 
+print(data.loc[data['TotalPayBenefits'].idxmax()]) # idxmax() returns the index of the max value
+
+print(data)
 print(data.describe().T)
 
+print(data[data['TotalPayBenefits'] == data['TotalPayBenefits'].min()]) # Returns the row with the lowest salary
+
+print(data.groupby('Year').mean()['BasePay']) # Returns the mean of the BasePay column grouped by year
+
+print(data['JobTitle'].nunique()) # Returns the number of unique job titles
+
+print(data['JobTitle'].value_counts().head(5)) # Returns the top 5 most common jobs
+
+print(sum(data[data['Year'] == 2013]['JobTitle'].value_counts() == 1)) # Returns the number of jobs that only appeared once in 2013
 
 
+def chief_string(title):
+    if 'chief' in title.lower():
+        return True
+    else:
+        return False
+
+print(sum(data['JobTitle'].apply(lambda x: chief_string(x)))) # Returns the number of people who have the word "chief" in their job title
+
+data['title_len'] = data['JobTitle'].apply(len) # Creates a new column called title_len that contains the length of the job title
+print(data[['title_len', 'TotalPayBenefits']].corr())
