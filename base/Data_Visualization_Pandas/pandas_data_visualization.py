e4b7d97b4cf88bfee0c15ce5f22339f65aae3f02
Rodolfo Bortoluzzi <qmclouca@gmail.com>
2023-02-10 13:42:06 -0300

    pandas plots2

--- base/Data_Visualization_Pandas/pandas_data_visualization.py
+++ base/Data_Visualization_Pandas/pandas_data_visualization.py
@@ -19,8 +19,8 @@ df2 = pd.read_csv('df2')
 
 #df1.plot.scatter(x='A', y='B', c='C', cmap='coolwarm')
 #df1.plot.scatter(x='A', y='B', s=df1['C']*50, cmap='coolwarm')
-df2.plot.box()
-df = pd.DataFrame(np.random.randn(1000, 2), columns=['a', 'b'])
-df.plot.hexbin(x='a', y='b', gridsize=25, cmap='coolwarm')
-
+#df2.plot.box()
+#df = pd.DataFrame(np.random.randn(1000, 2), columns=['a', 'b'])
+#df.plot.hexbin(x='a', y='b', gridsize=50, cmap='coolwarm')
+df2.plot.kde()
 # %%
